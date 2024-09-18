93795cd43b7679d22e996d4bf180fc24daa54a0d
Gabriel Cavalcanti <gabriel_sep@outlook.com>
2024-09-17 22:00:58 -0300

    feat: adiciona formulario de adicao de versao

---
+++ src/renderer/src/types/RunRealease.ts
@@ -0,0 +1,13 @@
+export type ReleaseOptions = {
+  firstRelease?: boolean;
+  prerelease?: 'alpha' | 'beta' | boolean; // Ex: 'alpha', 'beta', etc.
+  noVerify?: boolean;
+  skipChangelog?: boolean;
+  // Adicione mais opções conforme necessário
+};
+
+export type RunReleaseScriptParams = {
+  newVersionType: 'major' | 'minor' | 'path' | string; // Ex: 'major', 'minor', 'patch', ou uma versão específica '1.2.3'
+  workingDirectory: string; // Caminho para o diretório de trabalho
+  options?: ReleaseOptions; // Opções adicionais
+};
